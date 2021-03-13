faaac2d0ddae73091eae6e12526ead79c22ad509
Abhishek Ranjan <abhishek29ranjan@gmail.com>
2021-03-13 13:21:51 +0530

    hashed password

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,19 +1,16 @@
 const router = require('express').Router();
 const {User, validate} = require('../models/users');
-// const _ = require('lodash');
-
+const bcrypt = require('bcrypt');
 
 // registering user
-router.route('/').post((req, res) => {
+router.route('/').post(async (req, res) => {
   // validating fields
   const {error} = validate(req.body);
   if (error) return res.status(400).send(error.details[0].message);
 
   // finding whether user already exists
-  User.findOne({email: req.body.email})
-      .then((result) => {
-        return res.status(400).send('user already exists');
-      });
+  const result = await User.findOne({email: req.body.email});
+  if (result) return res.status(400).send('user already exists');
 
   // creating new user
   const user = new User({
@@ -21,9 +18,14 @@ router.route('/').post((req, res) => {
     email: req.body.email,
     password: req.body.password,
   });
+    // hashing password
+  const salt = await bcrypt.genSalt(10);
+  user.password = await bcrypt.hash(req.body.password, salt);
+
   // inserting into users collections
   user.save()
       .then((message) => res.send({
+        _id: message._id,
         name: message.name,
         email: message.email,
       }))
