a1fcfc77483ab50fc58b7fc5a80a6fc8232d5c9f
pranavjha1010 <pranavjha1010@gmail.com>
2021-03-16 18:26:01 +0530

    redux added

--- client/src/index.js
+++ client/src/index.js
@@ -4,10 +4,29 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import Reducer from './_reducers';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import promiseMiddleware from 'redux-promise';
+import ReduxThunk from 'redux-thunk';
+
+const createStoreWithMiddleware = applyMiddleware(
+    promiseMiddleware,
+    ReduxThunk,
+)(createStore);
+
 ReactDOM.render(
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>,
+    <Provider
+      store={createStoreWithMiddleware(
+          Reducer,
+          window.__REDUX_DEVTOOLS_EXTENSION__ &&
+        window.__REDUX_DEVTOOLS_EXTENSION__(),
+      )}
+    >
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </Provider>,
     document.getElementById('root'),
 );
 
