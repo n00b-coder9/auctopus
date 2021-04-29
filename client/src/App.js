ad46739116d53f4ea5807df2f415d87222030c21
Md Faiz Ansari <faizansari123z@gmail.com>
2021-04-29 23:00:46 +0530

    Faiz dashboard and Auth UI (#5)
    * Add: Dashboard with dummy data
    
    * Fixed merge conflicts
    
    * Fixed: minor issues
    
    * Fixed minor issues

--- client/src/App.js
+++ client/src/App.js
@@ -1,49 +1,54 @@
+/* eslint-disable no-unused-vars */
 import React, { Suspense } from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Auth from './hoc/auth';
 // import { makeStyles } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core';
+import { useSelector, useDispatch } from 'react-redux';
 // pages for this product
 import LandingPage from './components/views/LandingPage/LandingPage';
-import LoginPage from './components/views/LoginPage/LoginPage';
-import RegisterPage from './components/views/RegisterPage/RegisterPage.js';
+import LoginPage from './components/views/LoginPage';
+import RegisterPage from './components/views/RegisterPage';
 import UploadProductPage from './components/views/UploadProductPage/UploadProductPage';
 import Navigation from './components/Navigation';
 import Logout from './components/views/Logout';
 import Dashboard from './components/views/DashboardPage';
 import DetailProductPage from './components/views/DetailProductPage/DetailProductPage';
 import CardContainer from './components/views/LandingPage/Cards/CardContainer';
-
+import Snackbar from './components/common/Snackbar';
+import setIsSnackbarOpen from './_actions/snackbar_actions';
+import _ from 'underscore';
 // Styles for the whole App
-// const useStyles = makeStyles((theme) => ({
-//   root: {
-//     display: 'flex',
-//     flex: '1 0 auto',
-//   },
-//   toolbar: {
-//     display: 'flex',
-//     alignItems: 'center',
-//     justifyContent: 'flex-end',
-//     padding: theme.spacing(0, 1),
-//     // necessary for content to be below app bar
-//     ...theme.mixins.toolbar,
-//   },
-//   content: {
-//     display: 'flex',
-//     flex: '1 0 auto',
-//     flexDirection: 'column',
-//     padding: theme.spacing(3),
-//   },
-// }));
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+    flex: '1 0 auto',
+  },
+  toolbar: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'flex-end',
+    padding: theme.spacing(0, 1),
+    // necessary for content to be below app bar
+    ...theme.mixins.toolbar,
+  },
+  content: {
+    flex: 1,
+    padding: theme.spacing(3),
+  },
+}));
 function App() {
-  // const classes = useStyles();
+  const classes = useStyles();
+  const dispatch = useDispatch();
+  const snackbarState = useSelector((state) => state.snackbar, _.isEqual);
   return (
     <Suspense fallback={(<div>Loading...</div>)}>
-      <div className=''>
+      <div className={classes.root}>
         <Navigation />
 
         <main
-          className=''>
-          <div className='' />
+          className={classes.content}>
+          <div className={classes.toolbar} />
 
           {/* App content starts here */}
           <Switch>
@@ -55,8 +60,17 @@ function App() {
             <Route exact path="/product/upload" component={Auth(UploadProductPage, true)} />
             <Route exact path="/product/:productId" component={Auth(DetailProductPage, true)} />
             <Route exact path="/cards" component={CardContainer} />
+            <Route exact path='/product/:productid' component={Auth(DetailProductPage, true)} />
           </Switch>
         </main>
+        {/* App wide single snackbar */}
+        <Snackbar
+          open={snackbarState.isOpen}
+          message={snackbarState.message}
+          severity={snackbarState.severity}
+          onClose={
+            () => dispatch(setIsSnackbarOpen({ isOpen: false }))
+          } />
       </div>
     </Suspense>
   );
--- client/src/components/views/DashboardPage/index.js
+++ client/src/components/views/DashboardPage/index.js
@@ -1,28 +1,464 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
-import { makeStyles } from '@material-ui/core';
-const useStyles = makeStyles((theme) => ({
-  main: {
-    flex: 1,
-    flexDirection: 'row',
-  },
-  cells: {
-    flex: 1,
-  },
-}));
-const Dashboard = () => {
+import React, { useState, useEffect } from 'react';
+import {
+  Grid,
+  LinearProgress,
+  Select,
+  OutlinedInput,
+  MenuItem,
+  Button,
+} from '@material-ui/core';
+import { useTheme } from '@material-ui/styles';
+import {
+  ResponsiveContainer,
+  ComposedChart,
+  AreaChart,
+  LineChart,
+  Line,
+  Area,
+  PieChart,
+  Pie,
+  Cell,
+  YAxis,
+  XAxis,
+} from 'recharts';
+
+// styles
+import useStyles from './styles';
+
+// components
+import mock from './mock';
+import Widget from '../../utils/Widget/Widget';
+import PageTitle from '../../utils/PageTitle';
+import { Typography } from '../../utils/Wrappers';
+import Dot from '../../utils/Sidebar/components/Dot';
+import Table from './components/Table/Table';
+import BigStat from './components/BigStat/BigStat';
+import { withRouter } from 'react-router';
+
+const mainChartData = getMainChartData();
+const PieChartData = [
+  { name: 'Group A', value: 400, color: 'primary' },
+  { name: 'Group B', value: 300, color: 'secondary' },
+  { name: 'Group C', value: 300, color: 'warning' },
+  { name: 'Group D', value: 200, color: 'success' },
+];
+
+function Dashboard(props) {
   const classes = useStyles();
-  let user = useSelector((state) => state.user);
-  if (user) {
-    user = user.userData;
-  }
+  const theme = useTheme();
+
+  // local
+  const [mainChartState, setMainChartState] = useState('monthly');
+  useEffect(() => {
+    // code to call the data from backend
+  }, []);
   return (
-    <div className={classes.main}>
-      <p>Table</p>
-      <p>Graph_1</p>
-      <p>Graph_2</p>
+    <div style={{
+      flex: 1,
+    }}>
+      <PageTitle title="Dashboard" button={<Button
+        variant="contained"
+        size="medium"
+        color="secondary"
+      >
+        Upcoming Auctions
+      </Button>} />
+      <Grid container spacing={4}>
+        {/** This widget is common to all users will give an update on the
+           * total no of auctions that took place this week
+           *
+           * */}
+        <Grid item lg={3} md={4} sm={6} xs={12}>
+          <Widget
+            title="Sold This Week"
+            upperTitle
+            bodyClass={classes.fullHeightBody}
+            className={classes.card}
+          >
+            <div className={classes.visitsNumberContainer}>
+              <Grid container item alignItems={'center'}>
+                <Grid item xs={6}>
+                  <Typography size="xl" weight="medium" noWrap>
+                    12, 678
+                    {/* Count of items sold this week */}
+                  </Typography>
+                </Grid>
+                <Grid item xs={6}>
+                  {/* Chart to show the nos of items sold this week */}
+                  <LineChart
+                    width={100}
+                    height={30}
+                    data={[
+                      { value: 10 },
+                      { value: 15 },
+                      { value: 10 },
+                      { value: 17 },
+                      { value: 18 },
+                    ]}
+                  >
+                    <Line
+                      type="natural"
+                      dataKey="value"
+                      stroke={theme.palette.success.main}
+                      strokeWidth={2}
+                      dot={false}
+                    />
+                  </LineChart>
+                </Grid>
+              </Grid>
+            </div>
+            <Grid
+              container
+              direction="row"
+              justify="space-between"
+              alignItems="center"
+            >
+              <Grid item xs={4}>
+                <Typography color="text" colorBrightness="secondary" noWrap>
+                  Auctions
+                </Typography>
+                <Typography size="md">860{/* total no of auctions this week */}</Typography>
+              </Grid>
+              <Grid item xs={4}>
+                <Typography color="text" colorBrightness="secondary" noWrap>
+                  Sold
+                </Typography>
+                <Typography size="md">32{/* Total no of successful auctions */ }</Typography>
+              </Grid>
+              <Grid item xs={4}>
+                <Typography color="text" colorBrightness="secondary" noWrap>
+                  Average Cost
+                </Typography>
+                <Typography
+                  size="md">3.25%
+                  {/* Average Cost of items sold this week */}
+                </Typography>
+              </Grid>
+            </Grid>
+          </Widget>
+        </Grid>
+        <Grid item lg={3} md={8} sm={6} xs={12}>
+          <Widget
+            title="My Monthly Transactions"
+            upperTitle
+            className={classes.card}
+            bodyClass={classes.fullHeightBody}
+          >
+            <div className={classes.performanceLegendWrapper}>
+              <div className={classes.legendElement}>
+                <Dot color="warning" />
+                <Typography
+                  color="text"
+                  colorBrightness="secondary"
+                  className={classes.legendElementText}
+                >
+                  Items sold
+                </Typography>
+              </div>
+              <div className={classes.legendElement}>
+                <Dot color="primary" />
+                <Typography
+                  color="text"
+                  colorBrightness="secondary"
+                  className={classes.legendElementText}
+                >
+                  Items bought
+                </Typography>
+              </div>
+            </div>
+            <div className={classes.progressSection}>
+              <Typography
+                size="md"
+                color="text"
+                colorBrightness="secondary"
+                className={classes.progressSectionTitle}
+              >
+                Sold
+              </Typography>
+              <LinearProgress
+                variant="determinate"
+                // no of items the user has sold this month
+                value={77}
+                classes={{ barColorPrimary: classes.progressBarPrimary }}
+                className={classes.progress}
+              />
+            </div>
+            <div>
+              <Typography
+                size="md"
+                color="text"
+                colorBrightness="secondary"
+                className={classes.progressSectionTitle}
+              >
+                Bought
+              </Typography>
+              <LinearProgress
+                variant="determinate"
+                // no of items the user has bought this month
+                value={73}
+                classes={{ barColorPrimary: classes.progressBarWarning }}
+                className={classes.progress}
+              />
+            </div>
+          </Widget>
+        </Grid>
+        <Grid item lg={3} md={8} sm={6} xs={12}>
+          <Widget
+            title="Finances Overview"
+            upperTitle
+            className={classes.card}
+            bodyClass={classes.fullHeightBody}
+          >
+            <div className={classes.serverOverviewElement}>
+              <Typography
+                color="text"
+                colorBrightness="secondary"
+                className={classes.serverOverviewElementText}
+                noWrap
+              >
+                Spent
+              </Typography>
+              <div className={classes.serverOverviewElementChartWrapper}>
+                <ResponsiveContainer height={50} width="99%">
+                  {/* Feed the data with monthly expenditure */}
+                  <AreaChart data={getRandomData(10)}>
+                    <Area
+                      type="natural"
+                      dataKey="value"
+                      stroke={theme.palette.secondary.main}
+                      fill={theme.palette.secondary.light}
+                      strokeWidth={2}
+                      fillOpacity="0.25"
+                    />
+                  </AreaChart>
+                </ResponsiveContainer>
+              </div>
+            </div>
+            <div className={classes.serverOverviewElement}>
+              <Typography
+                color="text"
+                colorBrightness="secondary"
+                className={classes.serverOverviewElementText}
+                noWrap
+              >
+                Earnt
+              </Typography>
+              <div className={classes.serverOverviewElementChartWrapper}>
+                <ResponsiveContainer height={50} width="99%">
+                  {/* Feed the data with monthly expenditure */}
+                  <AreaChart data={getRandomData(10)}>
+                    <Area
+                      type="natural"
+                      dataKey="value"
+                      stroke={theme.palette.primary.main}
+                      fill={theme.palette.primary.light}
+                      strokeWidth={2}
+                      fillOpacity="0.25"
+                    />
+                  </AreaChart>
+                </ResponsiveContainer>
+              </div>
+            </div>
+          </Widget>
+        </Grid>
+        {/* This Grid will tell the no of transactions per category */}
+        <Grid item lg={3} md={4} sm={6} xs={12}>
+          <Widget title="Category Breakdown" upperTitle className={classes.card}>
+            <Grid container spacing={2}>
+              <Grid item xs={6}>
+                <ResponsiveContainer width="100%" height={144}>
+                  <PieChart>
+                    <Pie
+                      data={PieChartData}
+                      innerRadius={30}
+                      outerRadius={40}
+                      dataKey="value"
+                    >
+                      {PieChartData.map((entry, index) => (
+                        <Cell
+                          key={`cell-${index}`}
+                          fill={theme.palette[entry.color].main}
+                        />
+                      ))}
+                    </Pie>
+                  </PieChart>
+                </ResponsiveContainer>
+              </Grid>
+              <Grid item xs={6}>
+                <div className={classes.pieChartLegendWrapper}>
+                  {PieChartData.map(({ name, value, color }, index) => (
+                    <div key={color} className={classes.legendItemContainer}>
+                      <Dot color={color} />
+                      <Typography style={{ whiteSpace: 'nowrap', fontSize: 12 }} >
+                        &nbsp;{name}&nbsp;
+                      </Typography>
+                      <Typography color="text" colorBrightness="secondary">
+                        &nbsp;{value}
+                      </Typography>
+                    </div>
+                  ))}
+                </div>
+              </Grid>
+            </Grid>
+          </Widget>
+        </Grid>
+        {/**
+         * This line Chart will plot the no of items sold
+         *  and the no of items bought by the user daily */}
+        <Grid item xs={12}>
+          <Widget
+            bodyClass={classes.mainChartBody}
+            header={
+              <div className={classes.mainChartHeader}>
+                <Typography
+                  variant="h5"
+                  color="text"
+                  colorBrightness="secondary"
+                >
+                  Daily Line Chart
+                </Typography>
+                <div className={classes.mainChartHeaderLabels}>
+                  <div className={classes.mainChartHeaderLabel}>
+                    <Dot color="warning" />
+                    <Typography className={classes.mainChartLegentElement}>
+                      Sold
+                    </Typography>
+                  </div>
+                  <div className={classes.mainChartHeaderLabel}>
+                    <Dot color="primary" />
+                    <Typography className={classes.mainChartLegentElement}>
+                      Bought
+                    </Typography>
+                  </div>
+                </div>
+                {/* This option is used to the updates weekly or monthly or daily */}
+                <Select
+                  value={mainChartState}
+                  onChange={(e) => setMainChartState(e.target.value)}
+                  input={
+                    <OutlinedInput
+                      labelWidth={0}
+                      classes={{
+                        notchedOutline: classes.mainChartSelectRoot,
+                        input: classes.mainChartSelect,
+                      }}
+                    />
+                  }
+                  autoWidth
+                >
+                  <MenuItem value="daily">Daily</MenuItem>
+                  <MenuItem value="weekly">Weekly</MenuItem>
+                  <MenuItem value="monthly">Monthly</MenuItem>
+                </Select>
+              </div>
+            }
+          >
+            <ResponsiveContainer width="100%" minWidth={500} height={350}>
+              <ComposedChart
+                margin={{ top: 0, right: -15, left: -15, bottom: 0 }}
+                data={mainChartData}
+              >
+                <YAxis
+                  ticks={[0, 2500, 5000, 7500]}
+                  tick={{ fill: theme.palette.text.hint + '80', fontSize: 14 }}
+                  stroke={theme.palette.text.hint + '80'}
+                  tickLine={false}
+                />
+                <XAxis
+                  tickFormatter={(i) => i + 1}
+                  tick={{ fill: theme.palette.text.hint + '80', fontSize: 14 }}
+                  stroke={theme.palette.text.hint + '80'}
+                  tickLine={false}
+                />
+                <Area
+                  type="natural"
+                  dataKey="desktop"
+                  fill={theme.palette.background.light}
+                  strokeWidth={0}
+                  activeDot={false}
+                />
+                <Line
+                  type="natural"
+                  dataKey="mobile"
+                  stroke={theme.palette.primary.main}
+                  strokeWidth={2}
+                  dot={false}
+                  activeDot={false}
+                />
+                <Line
+                  type="linear"
+                  dataKey="tablet"
+                  stroke={theme.palette.warning.main}
+                  strokeWidth={2}
+                  dot={{
+                    stroke: theme.palette.warning.dark,
+                    strokeWidth: 2,
+                    fill: theme.palette.warning.main,
+                  }}
+                />
+              </ComposedChart>
+            </ResponsiveContainer>
+          </Widget>
+        </Grid>
+        {/* Now we set some stats related to the chart using our BigStat Component */}
+        {mock.bigStat.map((stat) => (
+          <Grid item md={4} sm={6} xs={12} key={stat.product}>
+            <BigStat {...stat} />
+          </Grid>
+        ))}
+        <Grid item xs={12}>
+          <Widget
+            title="My Items"
+            upperTitle
+            noBodyPadding
+            bodyClass={classes.tableWidget}
+          >
+            {/* Feed the data of my items to this table */}
+            <Table data={mock.table} />
+          </Widget>
+        </Grid>
+      </Grid>
     </div>
   );
-};
+}
 
-export default Dashboard;
+// #######################################################################
+function getRandomData(length, min, max, multiplier = 10, maxDiff = 10) {
+  const array = new Array(length).fill();
+  let lastValue;
+
+  return array.map((item, index) => {
+    let randomValue = Math.floor(Math.random() * multiplier + 1);
+
+    while (
+      randomValue <= min ||
+      randomValue >= max ||
+      (lastValue && randomValue - lastValue > maxDiff)
+    ) {
+      randomValue = Math.floor(Math.random() * multiplier + 1);
+    }
+
+    lastValue = randomValue;
+
+    return { value: randomValue };
+  });
+}
+
+function getMainChartData() {
+  const resultArray = [];
+  const tablet = getRandomData(31, 3500, 6500, 7500, 1000);
+  const desktop = getRandomData(31, 1500, 7500, 7500, 1500);
+  const mobile = getRandomData(31, 1500, 7500, 7500, 1500);
+
+  for (let i = 0; i < tablet.length; i++) {
+    resultArray.push({
+      tablet: tablet[i].value,
+      desktop: desktop[i].value,
+      mobile: mobile[i].value,
+    });
+  }
+
+  return resultArray;
+}
+
+export default withRouter(Dashboard);
